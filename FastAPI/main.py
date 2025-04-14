f0259b9875b9e2a8248c07b990be8a445540b2cb
JuanjoD06 <79176530+JuanjoD06@users.noreply.github.com>
2025-04-14 16:50:40 -0500

    Primeros pasos

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+
 @app.get("/")  #se hace get(obtiene) a la ruta "/"
 async def root(): #"async" con esta linea se especifica que sea asincrono (para poder llamar al servidor de la api)
     return {"message": "Hello World"}
+
+@app.get("/url")
+async def url():
+    return {"url_curso":"https://www.youtube.com/watch?v=8vX2g0c4Y1E&list=PLzH6i7a3j5qk9d2xG4b7f3m5e6n8Q0l4r&index=1"}
+
+#Inicia el server: uvicorn main:app --reload
+#uvicorn es un servidor asincro para ejecutar la api
+##main es el nombre del archivo
+##app es el nombre de la variable que contiene la instancia de FastAPI
+#/docs es la documentacion de la api
+#/redoc es la documentacion alternativa
